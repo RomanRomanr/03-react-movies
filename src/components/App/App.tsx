4e3cdbb44c4fdec2331e9a70149d7657a407426e
Roman <romario.higa@gmail.com>
2026-05-14 17:18:32 +0200

    2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,62 @@
+import { useState } from "react";
+import css from './App.module.css'
+import ErrorMessage from "../ErrorMessege/ErrorMessage";
+import Loader from "../Loader/Loade";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
+import SearchBar from "../SearchBar/SearchBar";
+import toast, { Toaster } from "react-hot-toast";
+import type { Movie } from "../../types/movie";
+import { FetchMovies } from "../../services/services";
+
 
 
 function App() {
-  
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [selectMovie, setSelectMovie] = useState<Movie | null>(null);
+  const close = () => setSelectMovie(null);
+  const search = async (query: string) => {
+    setLoader(true);
+    setMovies([]);
+    setError(null);
+    try {
+      const res = await FetchMovies({ query });
+      if (res.length === 0) {
+        toast.error("No movies found for your request.");
+        return;
+      }
+      setMovies(res);
+    } catch {
+      setError("There was an error, please try again...");
+    } finally {
+      setLoader(false);
+    }
+  };
+
   return (
-    
-    
+    <div className={css.app}>
+      <SearchBar onSubmit={search} />
+      {loader && <Loader />}
+
+      {error ? (
+        <ErrorMessage message={error} />
+      ) : (
+        !loader &&
+        movies.length > 0 && (
+          <MovieGrid
+            movies={movies}
+            onSelect={(movie) => setSelectMovie(movie)}
+          />
+        )
+      )}
+      {selectMovie && (
+        <MovieModal movie={selectMovie} onClose={close} />
+      )}
+
+      <Toaster position="top-center" />
+    </div>
   );
 }
 
